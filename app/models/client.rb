951e408dc9a1956bee0c795026e7bec84a608ef6
Aleksey Ivanov <aleksey.ivanov@infinto.com>
2020-09-14 16:30:30 +0300

    Deviseable concern

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,5 +1,3 @@
 class Client < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  include Deviseable
 end
